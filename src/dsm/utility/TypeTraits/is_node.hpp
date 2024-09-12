f04b5782d8dc6b2f38bbe08bdc1f5bf4657195c2
Simone Balducci <simone.balducci00@gmail.com>
2024-09-12 23:58:05 +0200

    Fix

--- src/dsm/utility/TypeTraits/is_node.hpp
+++ src/dsm/utility/TypeTraits/is_node.hpp
@@ -23,13 +23,13 @@ namespace dsm {
   template <typename Id, typename Size>
   struct is_node<const Node<Id, Size>&> : std::true_type {};
 
-  template <typename T>
-  struct is_node<std::is_base_of_v<Node<typename T::Id_t, typename T::Size_t>,
-                                   std::remove_const_t<std::remove_reference_t<T>>>>
-      : std::true_type {};
-
   template <typename Id, typename Size>
   struct is_node<std::unique_ptr<Node<Id, Size>>> : std::true_type {};
+
+  template <typename T>
+  inline constexpr bool is_node_v =
+      std::is_base_of_v<Node<typename T::Id_t, typename T::Size_t>,
+                        std::remove_const_t<std::remove_reference_t<T>>>;
 
   template <typename T>
   inline constexpr bool is_node_v = is_node<T>::value;
